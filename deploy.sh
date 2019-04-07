1c09a5fb1859b0b145780af69bf52912ae2fc9df
brett adcock <xbrett@github.com>
2019-04-07 17:23:31 -0400

    deploy setup

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4797
+export NODEBIN=`pwd`/assets/node_modules/.bin
+export PATH="$PATH:$NODEBIN"
+
+echo "Building..."
+
+mkdir -p ~/.config
+mkdir -p priv/static
+
+mix deps.get
+mix compile
+(cd assets && npm install)
+(cd assets && webpack --mode production)
+mix ecto.create
+mix ecto.migrate
+mix phx.digest
+
+echo "Generating release..."
+mix release
+
+echo "Stopping old copy of app, if any..."
+_build/prod/rel/eatOut/bin/eatOut stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/eatOut/bin/eatOut foreground
